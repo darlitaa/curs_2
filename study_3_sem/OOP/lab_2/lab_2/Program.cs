8f028228f2bd6f37328eb4ff7414d5fcfb5ab24e
Darya Litvinchuk <153180694+darlitaa@users.noreply.github.com>
2024-12-08 14:40:05 +0300

    Add files via upload

---
+++ study_3_sem/OOP/lab_2/lab_2/Program.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Linq;
+
+namespace lab_2
+{
+    internal class Program
+    {
+        partial class Car
+        {   
+            public const int YearIsNow = 2024;
+            public static int numberOfCars;
+            public readonly int Id;
+
+            string mark;
+            string model;
+            int yearOfRelease;
+            string color;
+            int price;
+            string registrationNumber;
+
+            public string Mark
+            {
+                get
+                {
+                    return mark;
+                }
+                set
+                {
+                    string[] marks = { "Toyota", "Honda", "Ford" };
+                    if (marks.Contains(value)) mark = value;
+                    else Console.WriteLine("такой марки машины нет");
+                }
+            }
+            public string Model
+            {
+                get
+                {
+                    return model;
+                }
+                set
+                {
+                    string[] models = { "Camry", "Civic", "Corolla", "Focus" };
+                    if (models.Contains(value)) model = value;
+                    else Console.WriteLine("такой модели машины нет");
+                }
+            }
+            public int YearOfRelease
+            {
+                get
+                {
+                    return yearOfRelease;
+                }
+                set
+                {
+                    if (yearOfRelease > 2025 && yearOfRelease < 0) Console.WriteLine("год выпуска не может быть больше 2024");
+                    else yearOfRelease = value;
+                }
+            }
+            public string Color
+            {
+                get
+                {
+                    return color;
+                }
+                set
+                {
+                    string[] сolors = { "Синий", "Красный", "Зеленый", "Черный", "Белый" };
+                    if (сolors.Contains(value)) color = value;
+                    else Console.WriteLine("такого цвета машины нет");
+                }
+            }
+            public int Price
+            {
+                get
+                {
+                    return price;
+                }
+                set
+                {
+                    if (price < 0) Console.WriteLine("Цена не может быть отрицательной");
+                    else price = value;
+                }
+            }
+            public string RegistrationNumber
+            {
+                get
+                {
+                    return registrationNumber;
+                }
+                set
+                {
+                    registrationNumber = value;
+                }
+            }
+
+            
+
+            static Car()
+            {
+                numberOfCars = 0;
+            }
+
+            
+            public Car(string mark, string model, int yearOfRelease, string color, int price, string registrationNumber)
+            {
+                this.mark = mark;
+                this.model = model;
+                this.yearOfRelease = yearOfRelease;
+                this.color = color;
+                this.price = price;
+                this.registrationNumber = registrationNumber;
+                numberOfCars++;
+                Id = GetHashCode();
+            }
+            
+            public Car()
+            {
+                mark = "Ford";
+                model = "Focus";
+                yearOfRelease = 2016;
+                color = "Черный";
+                price = 16800;
+                registrationNumber = "D012GH";
+                numberOfCars++;
+                Id = price * yearOfRelease;
+            }
+            
+            public Car(string mark, int yearOfRelease, string model, string color = "Белый", int price = 12000, string registrationNumber = "E345IJ")
+            {
+                this.mark = mark;
+                this.model = model;
+                this.yearOfRelease = yearOfRelease;
+                this.color = color;
+                this.price = price;
+                this.registrationNumber = registrationNumber;
+                numberOfCars++;
+                Id = GetHashCode();
+            }
+            
+
+            public static void GetCarAge(ref int YearIsNow, int YearOfRelease, out int carAge)
+            {
+                carAge = YearIsNow - YearOfRelease;
+            }
+            public static void ShowStatic()
+            {
+                Console.WriteLine($"Общее количество автомобилей: {numberOfCars}");
+            }
+        }
+
+        class Test
+        {
+            public int test;
+            private Test()
+            {
+                test = 13;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Car[] cars = new Car[]
+            {
+                new Car("Toyota", "Camry", 2018, "Синий", 20000, "A123BC"),
+                new Car("Honda", "Civic", 2017, "Красный", 18000, "B456CD"),
+                new Car("Toyota", "Corolla", 2015, "Зеленый", 15000, "C789EF"),
+                new Car(),
+                new Car("Honda", 2012,"Civic"),
+            };
+
+            foreach (var car in cars)
+            {
+                Console.WriteLine($"id: {car.Id} {car.Mark} {car.Model} {car.YearOfRelease} {car.Color} {car.Price} {car.RegistrationNumber}");
+            }
+
+            Console.WriteLine($"\nВывод стоки:{cars[1].YearOfRelease.ToString()}");
+            Console.WriteLine($"Сравнение:{cars[1].YearOfRelease.Equals(cars[2].YearOfRelease)}");
+            Console.WriteLine($"Проверка типа:{cars[1].YearOfRelease.GetType()}");
+
+            
+
+            int currentYear = Car.YearIsNow;
+            Console.Write("\nВведите ID машины, чтобы узнать её возраст: ");
+
+            if (int.TryParse(Console.ReadLine(), out int GetID))
+            {
+                var car = Array.Find(cars, c => c.Id == GetID);
+                if (car != null)
+                {
+                    Car.GetCarAge(ref currentYear, car.YearOfRelease, out int age);
+                    Console.WriteLine($"Возраст машины: {age} лет ({car.Mark} {car.Model} {car.YearOfRelease} {car.Color} {car.Price} {car.RegistrationNumber})");
+                }
+                else Console.WriteLine("Машина с таким ID не найдена");
+            }
+            else Console.WriteLine("Некорректный ввод");
+
+            
+
+            Console.Write("\nВведите марку машины (Toyota, Honda, Ford): ");
+            var neededMark = Console.ReadLine();
+
+            foreach (var car in cars)
+            {
+                if (neededMark == car.Mark)
+                {
+                    Console.WriteLine($"id: {car.Id} {car.Mark} {car.Model} {car.YearOfRelease} {car.Color} {car.Price} {car.RegistrationNumber}");
+                }
+            }
+
+            
+
+            Console.WriteLine("\nВведите модель машины (Camry, Civic, Corolla, Focus) и возраст эксплуатации (выведится от введенного и до max): ");
+            var neededModel = Console.ReadLine();
+            int.TryParse(Console.ReadLine(), out int ageOfOperation);
+
+            foreach (var car in cars)
+            {
+                Car.GetCarAge(ref currentYear, car.YearOfRelease, out int age);
+                if (neededModel == car.Model && age >= ageOfOperation)
+                {
+                    Console.WriteLine($"id: {car.Id} {car.Mark} {car.Model} {car.YearOfRelease} {car.Color} {car.Price} {car.RegistrationNumber}");
+                }
+            }
+
+        }
+    }
+}
---
+++ study_3_sem/OOP/lab_3/lab_3/Program.cs
@@ -0,0 +1,266 @@
+﻿using System;
+
+namespace lab_3
+{
+    public class Node
+    {
+        public int Data;
+        public Node Next;
+        public Node(int data)
+        {
+            Data = data;
+            Next = null;
+        }
+    }
+
+    public class List
+    {
+        public Node head = null;
+        public void Add(int date)
+        {
+            Node newNode = new Node(date);
+            if (head == null)
+            {
+                head = newNode;
+            }
+            else
+            {
+                Node current = head;
+                while (current.Next != null)
+                {
+                    current = current.Next;
+                }
+                current.Next = newNode;
+            }
+        }
+
+        public void Print()
+        {
+            Node current = head;
+            while (current != null)
+            {
+                Console.Write(current.Data + " ");
+                current = current.Next;
+            }
+            Console.WriteLine();
+        }
+
+        public static List operator +(List list1, List list2)
+        {
+            List newList = new List();
+            Node current;
+
+            current = list1.head;
+            while(current != null)
+            {
+                newList.Add(current.Data);
+                current = current.Next;
+            }
+            current = list2.head;
+            while (current != null)
+            {
+                newList.Add(current.Data);
+                current = current.Next;
+            }
+            return newList;
+        }
+
+        public static List operator --(List list)
+        {
+            if (list.head == null) return list;
+
+            list.head = list.head.Next;
+            return list;
+        }
+
+        public static bool operator ==(List list1, List list2)
+        {
+            Node current1 = list1.head;
+            Node current2 = list2.head;
+
+            while (current1 != null && current2 != null)
+            {
+                if (current1.Data != current2.Data)
+                {
+                    return false;
+                }
+                current1 = current1.Next;
+                current2 = current2.Next;
+            }
+            return current1 == null && current2 == null;
+        }
+        public static bool operator !=(List list1, List list2)
+        {
+            return !(list1 == list2);
+        }
+
+        public static implicit operator bool(List list)
+        {
+            return list.head == null;
+        }
+
+        public class Production
+        {
+            Guid Id { get; set; }
+            string organizationName { get; set; }
+
+            public Production(string name)
+            {
+                Id = Guid.NewGuid();
+                organizationName = name;
+            }
+        }
+        public class Developer
+        {
+            string fullName { get; set; }
+            Guid Id { get; set; }
+            string department { get; set; }
+
+            public Developer(string name, string dep)
+            {
+                fullName = name;
+                Id = Guid.NewGuid();
+                department = dep;
+            }
+        }
+    }
+
+    public static class StatisticOperation
+    {
+        public static int GetSum(List list)
+        {
+            Node current = list.head;
+            int sum = 0;
+            while (current != null)
+            {
+                sum += current.Data;
+                current = current.Next;
+            }
+            return sum;
+        }
+
+        public static int GetDifferenceMinMax(List list)
+        {
+            Node current = list.head;
+            int max = current.Data;
+            int min = current.Data;
+            while (current != null)
+            {
+                if (max < current.Data) max = current.Data;
+                if (min > current.Data) min = current.Data;
+                current = current.Next;
+            }
+            return max - min;
+        }
+
+        public static int NumberOfElements(List list)
+        {
+            Node current = list.head;
+            int count = 0;
+            while (current != null)
+            {
+                count++;
+                current = current.Next;
+            }
+            return count;
+        }
+
+        public static int? LastNumber(this string str)
+        {
+            var parts = str.Split(new[] { ' ' });
+            for (int i = parts.Length - 1; i >= 0; i--)
+            {
+                if (int.TryParse(parts[i], out var number))
+                {
+                    return number;
+                }
+            }
+            return null;
+        }
+
+        public static bool Remove(this List list, int date)
+        {
+            if (list.head == null) return false;
+
+            if (list.head.Data == date)
+            {
+                list.head = list.head.Next;
+                return true;
+            }
+
+            Node current = list.head;
+            while (current.Next != null)
+            {
+                if (current.Next.Data == date)
+                {
+                    current.Next = current.Next.Next;
+                    return true;
+                }
+                current = current.Next;
+            }
+            return false;
+        }
+    }
+    class Program
+    {
+        static void Main()
+        {
+            List list1 = new List();
+            list1.Add(1);
+            list1.Add(2);
+            list1.Add(3);
+            list1.Add(4);
+            list1.Add(5);
+            list1.Add(6);
+
+            List list2 = new List();
+            list2.Add(7);
+            list2.Add(8);
+            list2.Add(9);
+
+            Console.Write("Первый список: ");
+            list1.Print();
+            Console.Write("Второй список: ");
+            list2.Print();
+            Console.WriteLine();
+
+
+            Console.WriteLine("\tПерегруженные операторы: ");
+
+            Console.Write("Объеденение двух списков: ");
+            List combinedList = list1 + list2;
+            combinedList.Print();
+
+            Console.Write("Удаление элемента из начала списка: ");
+            --list1;
+            list1.Print();
+
+            bool areEqual = list1 == list2;
+            Console.WriteLine($"Проверка равны ли списки: {areEqual}");
+
+            bool isEmpty = new List();
+            Console.WriteLine($"Проверка пуст ли список: {isEmpty}");
+            Console.WriteLine();
+
+            Console.WriteLine("\tМетоды расширения: ");
+
+            string str = "74 42 83 23 88";
+            int? lastNumber = StatisticOperation.LastNumber(str);
+            Console.WriteLine($"Последнее число в строке: {lastNumber}");
+
+            list1.Remove(5);
+            Console.Write($"Убрали элемент 5 из односвязного списка: ");
+            list1.Print();
+            Console.WriteLine();
+
+
+            List.Production myProduction = new List.Production("BSTU");
+            List.Developer myDeveloper = new List.Developer("Litvinchuk Daria Valeryevna", "FIT");
+
+            Console.WriteLine("\tMетоды для работы с классом: ");
+            Console.WriteLine($"Сумма элементов cписка: {StatisticOperation.GetSum(list2)}");
+            Console.WriteLine($"Pазница между максимальным и минимальным элементов cписка: {StatisticOperation.GetDifferenceMinMax(list2)}");
+            Console.WriteLine($"Количество элементов в списке: {StatisticOperation.NumberOfElements(list2)}");
+        }
+    }
+}
