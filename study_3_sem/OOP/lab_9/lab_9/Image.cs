56610e3fd2fef0ffd39106a5f0257d07fcb0274b
Darya Litvinchuk <153180694+darlitaa@users.noreply.github.com>
2024-12-14 09:51:53 +0300

    Add files via upload

---
+++ study_3_sem/OOP/lab_9/lab_9/Image.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections;
+
+namespace lab_9
+{
+    public class Image<T> : ISet<T>
+    {
+        private T[] _image = new T[10];
+        private int _count;
+        public Image()
+        {
+            _count = 0;
+        }
+
+        public int Count { get { return _count; } }
+
+        public bool IsReadOnly { get { return false; } }
+
+        public bool Add(T item)
+        {
+            if (_count < _image.Length)
+            {
+                _image[_count] = item;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void Clear()
+        {
+            _count = 0;
+        }
+
+        public bool Contains(T item)
+        {
+            bool inObj = false;
+            for (int i = 0; i < _count; i++)
+            {
+                if (_image[i].Equals(item))
+                {
+                    inObj = true;
+                    break;
+                }
+            }
+            return inObj;
+        }
+
+        #region
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ExceptWith(IEnumerable<T> other)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void IntersectWith(IEnumerable<T> other)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsProperSubsetOf(IEnumerable<T> other)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsProperSupersetOf(IEnumerable<T> other)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsSubsetOf(IEnumerable<T> other)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsSupersetOf(IEnumerable<T> other)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Overlaps(IEnumerable<T> other)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool SetEquals(IEnumerable<T> other)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SymmetricExceptWith(IEnumerable<T> other)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UnionWith(IEnumerable<T> other)
+        {
+            throw new NotImplementedException();
+        }
+
+        void ICollection<T>.Add(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+        #endregion 
+    }
+}
---
+++ study_3_sem/OOP/lab_9/lab_9/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+
+namespace lab_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Image<int> image1 = new Image<int>();
+            Image<int> image2 = new Image<int>();
+            Image<int> image3 = new Image<int>();
+            Image<int> image4 = new Image<int>();
+            HashSet<Image<int>> set = new() {image1, image2, image3, image4 };
+
+            // task 2
+            var employees = new List<string> { "Tom", "Sam", "Bob", "Nik", "Pasha", "Slava" };
+
+            LinkedList<string> people = new LinkedList<string>(employees);
+            foreach (string person in people)
+            {
+                Console.WriteLine(person);
+            }
+            Console.WriteLine("------------------");
+
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                people.RemoveFirst();
+            }
+
+            people.AddFirst("Mark");
+            people.AddLast("Oleg");
+            LinkedListNode<string>? current = people.FindLast("Nik");
+            people.AddAfter(current, "Anna");
+            people.AddBefore(current, "Masha");
+
+            foreach (string person in people)
+            {
+                Console.WriteLine(person);
+            }
+            Console.WriteLine("------------------");
+
+            HashSet<string> set2 = new HashSet<string>() {
+            "Mama", "Papa", "Son"
+        };
+
+            set2.UnionWith(people);
+
+            foreach (string person in set2)
+            {
+                Console.WriteLine(person);
+            }
+            Console.WriteLine("------------------");
+            string searchValue = "Mama";
+
+            bool containsValue = set2.Contains(searchValue);
+            Console.WriteLine(containsValue);
+
+
+            Console.WriteLine("------------------");
+            // Task 3
+
+            var MyColletion = new ObservableCollection<Image<int>>();
+
+            MyColletion.CollectionChanged += MyCollection_onChange; // подписываемся на изменения коллекции 
+
+            MyColletion.Add(image1);
+
+            MyColletion.RemoveAt(0);
+
+
+
+
+
+            Console.ReadKey();
+        }
+
+        private static void MyCollection_onChange(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            switch (e.Action)
+            {
+                case NotifyCollectionChangedAction.Add:
+                    Console.WriteLine("Add element in collection MyCollection");
+                    break;
+                case NotifyCollectionChangedAction.Remove:
+                    Console.WriteLine("Delete element in collection MyCollection");
+                    break;
+                case NotifyCollectionChangedAction.Replace:
+                    Console.WriteLine("Change element in collection MyCollection");
+                    break;
+            }
+        }
+    }
+}
