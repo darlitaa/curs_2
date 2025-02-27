646105c65ef7759ea70788db8999fd6c2b022655
Darya Litvinchuk <153180694+darlitaa@users.noreply.github.com>
2025-02-27 12:03:55 +0300

    Add files via upload

---
+++ study_4_sem/TPvI/lab02/ASPA/ASPA002_1/Program.cs
@@ -0,0 +1,22 @@
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+app.UseRouting();
+app.UseStaticFiles();
+
+app.MapGet("/aspnetcore", () => "Привет, ASP.NET Core!");
+
+app.MapGet("/", async context =>
+{
+    context.Response.Redirect("/Index.html");
+});
+
+app.MapGet("/ASPA002", async context =>
+{
+    context.Response.ContentType = "image/jpg";
+    await context.Response.SendFileAsync("wwwroot/jpg/Neumann.jpg");
+});
+
+app.Run();
