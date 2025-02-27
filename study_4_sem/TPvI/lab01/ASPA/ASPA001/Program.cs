807aaf703eb85e957183b313f02797749a717bae
Darya Litvinchuk <153180694+darlitaa@users.noreply.github.com>
2025-02-27 11:57:04 +0300

    Add files via upload

---
+++ study_4_sem/TPvI/lab01/ASPA/ASPA001/Program.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.HttpLogging;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddHttpLogging(o => {
+            o.LoggingFields = HttpLoggingFields.RequestMethod |
+            HttpLoggingFields.RequestPath |
+            HttpLoggingFields.ResponseStatusCode;
+        });
+
+        builder.Logging.AddFilter("Microsoft.AspNetCore.HttpLogging", LogLevel.Information);
+        var app = builder.Build();
+
+        app.UseHttpLogging();
+        app.MapGet("/", () => "моё первое ASPA");
+
+        app.Run();
+    }
+}
