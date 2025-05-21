6e09565ece69d8e30f3b8b0cacbb7eb9728f44f8
Darya Litvinchuk <153180694+darlitaa@users.noreply.github.com>
2025-05-21 12:22:18 +0300

    Add files via upload

---
+++ study_4_sem/SiAP (JS)/lab/lab11/src/features/posts/Posts.tsx
@@ -0,0 +1,25 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchPostsThunk } from './postsSlice';
+import { RootState, AppDispatch } from '../../app/store';
+import PostItem from '../../components/PostItem';
+import { Post } from './postsAPI';
+
+const Posts = () => {
+  const dispatch = useDispatch<AppDispatch>();
+  const posts = useSelector((state: RootState) => state.posts.posts);
+
+  useEffect(() => {
+    dispatch(fetchPostsThunk());
+  }, [dispatch]);
+
+  return (
+    <div>
+      {posts.map((post: Post) => (
+        <PostItem key={post.id} post={post} />
+      ))}
+    </div>
+  );
+};
+
+export default Posts;
